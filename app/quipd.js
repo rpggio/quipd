1997bb9def07db18ac93590a4ad82b581519edc0
testUser92121 <password92121>
2015-06-27 08:12:39 +0200

    Quip sort order

--- app/quipd.js
+++ app/quipd.js
@@ -1,74 +1,92 @@
-
 Quips = new Mongo.Collection("quips");
 
 var scrollHistory = function() {
-    var history = $('#history');
-    var scrollHeight = history.prop('scrollHeight');
-    console.log('scroll height: ' + scrollHeight);
-    history.scrollTop(scrollHeight);
+  var history = $('#history');
+  var scrollHeight = history.prop('scrollHeight');
+  history.scrollTop(scrollHeight);
 }
 
-if (Meteor.isServer) {
- 
-  Meteor.startup(function () {
+var browseMode = false;
 
-      return Meteor.methods({
-        resetQuips: function() {
-          Quips.remove({});
-          return Quips.insert({name: "started up! " + Date()});
-        }
-      });
+if (Meteor.isServer) {
+
+  Meteor.startup(function() {
+
+    return Meteor.methods({
+      resetQuips: function() {
+        Quips.remove({});
+        return Quips.insert({
+          text: "started up! " + Date(),
+          createdAt: Date()
+        });
+      }
+    });
 
   });
 
   Meteor.publish('quipsPub', function(limit) {
-    return Quips.find({}, { limit: limit });
+    return Quips.find({}, {
+      limit: limit || 10,
+      sort: {
+        createdAt: -1
+      }
+    });
   });
- 
+
 } else if (Meteor.isClient) {
- 
-  var QUIPS_INCREMENT = 8;
+
+  var QUIPS_INCREMENT = 10;
   Session.setDefault('quipsLimit', QUIPS_INCREMENT);
   Deps.autorun(function() {
-    Meteor.subscribe('quipsPub', Session.get('quipsLimit'), function(e){
-      scrollHistory();
+    Meteor.subscribe('quipsPub', Session.get('quipsLimit'), function(e) {
+      if (!browseMode) {
+        scrollHistory();
+      }
     });
   });
 
   Template.quipdMain.helpers({
     quips: function() {
-      return Quips.find();
+      return Quips.find({}, {
+        sort: {
+          createdAt: 1
+        }
+      });
     },
     moreResults: function() {
-        var quipsCount = Quips.find().count();
-        var limit = Session.get("quipsLimit");
-        var result = !(quipsCount < limit);
-        console.log([quipsCount, limit, result])
-        return result;
+      var quipsCount = Quips.find().count();
+      var limit = Session.get("quipsLimit");
+      var result = !(quipsCount < limit);
+      return result;
     }
   });
- 
+
   Template.quipdMain.events({
-      'click #showMore': function(){
-            Session.set("quipsLimit",
-               Session.get("quipsLimit") + QUIPS_INCREMENT);
-      },
-      'submit .new-quip': function (event) {
-         var text = event.target.quipText.value;
-         console.log('creating ' + text);
-         Quips.insert({ text: text });
-         event.target.quipText.value = "";
-         scrollHistory();
-         return false;
-      },
-      'click #resetQuips': function(){
-        Meteor.call('resetQuips');
-      }
+    'click #showMore': function() {
+      browseMode = true;
+      Session.set("quipsLimit",
+        Session.get("quipsLimit") + QUIPS_INCREMENT);
+    },
+    'submit .new-quip': function(event) {
+      browseMode = false;
+      var text = event.target.quipText.value;
+      console.log('creating ' + text);
+      Quips.insert({
+        text: text,
+        createdAt: Date()
+      });
+      event.target.quipText.value = "";
+      scrollHistory();
+      return false;
+    },
+    'click #resetQuips': function() {
+      Meteor.call('resetQuips');
+    }
   });
 
-  Template.quipdMain.rendered = function(){
-      Session.set("quipsLimit", QUIPS_INCREMENT);
-      scrollHistory();
+  Template.quipdMain.rendered = function() {
+    Session.set("quipsLimit", QUIPS_INCREMENT);
+    scrollHistory();
   };
 
 }
