c018bd90725599a5b0713d8afb94472464c31018
Ryan Smith <ryan.smith@pobox.com>
2015-11-27 13:21:32 +0100

    Basic quip styling

--- app_2/client/body/main/quips/quips.js
+++ app_2/client/body/main/quips/quips.js
@@ -3,11 +3,20 @@ Template.quips.viewmodel({
 	quips: function() {
 		return Quips.find();
 	},
-	add: function() {
+	addQuip: function() {
 		var quip = {
 			text: this.newText()
 		};
 		Quips.insert(quip);
 		this.newText('');
-	}
+	},
+	events: {
+	    'keydown .add-quip': function(event, templateInstance) {
+	    	switch (event.which) {
+		        case 13:   // enter
+		          this.addQuip();
+		          return;
+	    	}
+	   	}
+    }
 });
